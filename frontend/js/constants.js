2deae0a6532ca99d20b2c1cc793c781a53b0c368
Not at all cats <thepetcatsnft@gmail.com>
2022-06-06 20:11:06 -0400

    updated

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -3,23 +3,13 @@ const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Meoowww!! Welcome to the Not At All Cats NFT Project!!";
 const welcome_h2 = "Connect to MetaMask or get meowed at, Grrrr!";
-const welcome_p = 'WHAT YOU ARE LOOKIN AT, THEY AINT NO CATS!! HANG ON TO FIND OUT WHAT THEY ARE!!';
-const h1_presale_coming_soon = "Coming Soon!!";
-const h1_presale_mint = "Pre-Sale Minting Open!!";
+const welcome_p = 'Welcome! You are looking at the new Generation of cats that are not really cats. These Multi Functional Beings are here to help the humanity continue to exist in this new Metaworld.';
 const h1_public_mint = "Public Minting Open!!";
-const h2_presale_coming_soon = "Pre-Sale Minting Countdown";
 const h2_presale_mint = "Public Minting Countdown";
-const p_presale_coming_soon = "We are working hard to launch the NFT Collection. Stay tuned for updates!";
-const p_presale_mint_not_whitelisted = "You are not whitelisted for the pre-sale.. 😢";
-const p_presale_mint_whitelisted = "You're on the whitelist for the pre-sale! 🎉";
-const p_presale_mint_already_minted = "You've already claimed your whitelist mint. Thank you! 🎉";
-const p_public_mint = "No whitelist needed. Public minting is now open! 🎉";
-const button_presale_coming_soon = "Get on the Whitelist";
-const button_presale_mint_whitelisted = "Mint Your Special NFT";
-const button_presale_mint_not_whitelisted = "Get on the Whitelist";
+const p_public_mint = "Public minting is now open! 🎉";
 const button_presale_already_minted = "Join The Community";
 const button_public_mint = "Mint Your NFT";
-const mint_failed = "Minting failed. 😢 Please try again.";
+const mint_failed = "Minting failed. Please try again.";
 
 const abi = [
   {
